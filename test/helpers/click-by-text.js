edf32bfe29c28391d3ceda24279ecbaed296d98f
Alan Shaw <alan@tableflip.io>
2015-07-20 14:58:42 +0100

    Completed end to end class book test

---
+++ test/helpers/click-by-text.js
@@ -0,0 +1,12 @@
+module.exports = function (selector, text, browser) {
+  return browser.execute(function (selector, text) {
+    var elements = document.querySelectorAll(selector)
+
+    console.log(selector, text, elements.length)
+
+    for (var i = 0; i < elements.length; ++i) {
+      var textContent = elements[i].textContent
+      if (textContent === text) return elements[i].click()
+    }
+  }, [selector, text])
+}
--- test/nightwatch/book-class.js
+++ test/nightwatch/book-class.js
@@ -1,5 +1,6 @@
 var config = require('config')
 var moment = require('moment')
+var Faker = require('faker')
 var login = require('../helpers/bo/login')
 var fakeRecipe = require('sow-api/test/helpers/fake-recipe')
 var createRecipe = require('../helpers/bo/create-recipe')
@@ -11,6 +12,7 @@ var fakeClassTemplate = require('sow-api/test/helpers/fake-class-template')
 var createClassTemplate = require('../helpers/bo/create-class-template')
 var fakeClass = require('sow-api/test/helpers/fake-class')
 var createClass = require('../helpers/bo/create-class')
+var clickByText = require('../helpers/click-by-text')
 
 var recipes = [fakeRecipe(), fakeRecipe()]
 var menu = fakeMenu()
@@ -35,13 +37,56 @@ module.exports = {
       .waitForElementVisible('body.home', 1000)
       .url(config.web.url + '/classes')
       .waitForElementVisible('body.classes', 1000)
-      .click('#date option[value=' + moment(cls.startTimes[0]).format('YYYY-MM') + ']')
+      .click('#date option[value="' + moment(cls.startTimes[0]).format('YYYY-MM') + '"]')
       .waitForElementVisible('body.classes', 1000)
-      .useXpath()
-      .click('//span[text()="' + cls.name + '"]')
-      .useCss()
+
+    browser = clickByText('.sow-panel span', classTemplate.name, browser)
       .waitForElementVisible('body.class', 1000)
-      .click('.btn.book')
+      .click('.btn.book-now')
       .waitForElementVisible('body.basket', 1000)
+      .assert.containsText('#basket tr:first-child .product-description p:first-child', classTemplate.name)
+  },
+  '03 - Checkout booking info': function (browser) {
+    browser = browser
+      .click('.btn-go-to-payment')
+      .waitForElementVisible('body.booking-info', 1000)
+      .click('input[type="checkbox"][name="classes[0]"]')
+      .click('input[type="checkbox"][name="classes[0]"]')
+      .setValue('#firstName', Faker.name.firstName())
+      .setValue('#lastName', Faker.name.lastName())
+      .setValue('#email', Faker.internet.email())
+      .setValue('#tel', Faker.phone.phoneNumber())
+      .click('[name="dietNone"]')
+      .click('.btn-confirm-attendee')
+      .pause(1000)
+      .click('#modal-diet-requirements-anything button')
+      .pause(1000)
+      .click('.btn[href="/checkout/billing-and-shipping"]')
+      .waitForElementVisible('body.billing-and-shipping', 1000)
+  },
+  '04 - Checkout billing and shipping': function (browser) {
+    browser = browser
+      .setValue('#billingAddress1', Faker.address.streetAddress())
+      .setValue('#billingCity', Faker.address.streetAddress())
+      .setValue('#billingCounty', Faker.address.streetAddress())
+      .setValue('#billingPostcode', Faker.address.streetAddress())
+      .click('.btn')
+      .waitForElementVisible('body.payment', 1000)
+  },
+  '05 - Checkout payment': function (browser) {
+    browser = browser
+      .pause(1000) // No idea, needed for next setValue to work
+      .setValue('#cardNumber', '4242424242424242')
+      .setValue('#cvc', Faker.finance.account(3))
+      .setValue('#expMonth', moment().month())
+      .setValue('#expYear', moment().add(1, 'year').year())
+      .click('.btn')
+      .waitForElementVisible('body.confirm-order', 5000)
+  },
+  '06 - Checkout confirm': function (browser) {
+    browser = browser
+      .click('.btn')
+      .waitForElementVisible('body.thanks', 1000)
+      .end()
   }
 }
