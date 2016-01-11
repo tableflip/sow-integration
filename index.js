8acc6414b7625b4dbd70c7f2de930c50e3d933d8
Liam Wooding <liamwooding@gmail.com>
2016-01-11 15:19:09 +0000

    Fixes problem where frontend wouldn't stop at end of tests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require('fs')
 var path = require('path')
 var spawn = require('child_process').spawn
 var chalk = require('chalk')
+var psTree = require('ps-tree')
+var async = require('async')
 
 // TODO: config
 var baseDir = process.env.BASE_DIR || path.normalize(path.join(__dirname, '..'))
@@ -22,6 +24,7 @@ module.exports = function (done) {
 }
 module.exports.apps = apps
 module.exports.checkForMissingApps = checkForMissingApps
+module.exports.stop = stop
 
 function checkForMissingApps () {
   var missing = apps.filter(isMissing)
@@ -42,6 +45,7 @@ function startAllApps (done) {
     app.proc = spawn('npm', ['start'], {cwd: app.root, env: env})
     console.log(app.name, 'starting')
     app.proc.stdout.on('data', function (data) {
+      console.log(app.name, data.toString('utf8'))
       if (app.ready || !app.waitFor) console.log(app.name, data.toString('utf8'))
       if (data.toString('utf8').indexOf(app.waitFor) === -1) return
       app.ready = true
@@ -53,6 +57,31 @@ function startAllApps (done) {
     app.proc.stderr.on('data', function (data) {
       console.error(chalk.red(app.name, data.toString('utf8')))
     })
+  })
+}
+
+function stop (done) {
+  async.each(apps, function (app, cb) {
+    kill(app.proc.pid, cb)
+  }, done)
+}
+
+function kill (pid, cb) {
+  psTree(pid, function (err, children) {
+    if (err) return cb(err)
+
+    var errs = []
+    var pids = [pid].concat(children.map(function (p) { return p.PID }))
+
+    pids.forEach(function (tpid) {
+      try {
+        process.kill(tpid, 'SIGTERM')
+      } catch (err) {
+        errs.push(err)
+      }
+    })
+
+    cb(errs.length ? errs : null)
   })
 }
 
--- test.js
+++ test.js
@@ -3,6 +3,12 @@ var spawn = require('child_process').spawn
 
 runner(function () {
   console.log('starting nightwatch')
-  spawn('node_modules/.bin/nightwatch', [], { cwd: __dirname, stdio: 'inherit' })
-    .on('exit', process.exit)
+
+  var proc = spawn('node_modules/.bin/nightwatch', [], {cwd: __dirname, stdio: 'inherit'})
+
+  proc.on('close', function () {
+    runner.stop(function (err) {
+      if (err) throw err
+    })
+  })
 })
