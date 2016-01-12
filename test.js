4ac80e97bc9015fb85071a8b23d0d9983fd85ca7
Liam Wooding <liamwooding@gmail.com>
2016-01-12 12:38:43 +0000

    Adds multiple voucher booking test

--- test.js
+++ test.js
@@ -4,7 +4,21 @@ var spawn = require('child_process').spawn
 runner(function () {
   console.log('starting nightwatch')
 
-  var proc = spawn('node_modules/.bin/nightwatch', [], {cwd: __dirname, stdio: 'inherit'})
+  var args = []
+
+  if (process.env.NIGHTWATCH_ENV) {
+    args = args.concat('--env', process.env.NIGHTWATCH_ENV)
+  }
+
+  if (process.env.NIGHTWATCH_TEST) {
+    args = args.concat('--test', process.env.NIGHTWATCH_TEST)
+  }
+
+  if (process.env.NIGHTWATCH_TEST_CASE) {
+    args = args.concat('--testcase', process.env.NIGHTWATCH_TEST_CASE)
+  }
+
+  var proc = spawn('node_modules/.bin/nightwatch', args, {cwd: __dirname, stdio: 'inherit'})
 
   proc.on('close', function () {
     runner.stop(function (err) {
